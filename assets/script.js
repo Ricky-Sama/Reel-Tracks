d142d6003e47b677e0cee1688cfdd7e56215bbdd
Jesse <jesse.lare@gmail.com>
2023-08-13 16:25:23 -0400

    JL pseudo code

--- assets/script.js
+++ assets/script.js
@@ -21,3 +21,33 @@ async function getMovieData(movieTitle) {
  
     // https://www.omdbapi.com/?s=thor&page=1&apikey=3497cfa
 
+/*
+
+When the 'Search' button is clicked: (example: function whenSearchButtonClicked() {};)
+    Get the movie title input by the user.
+    
+    If the movie title input is empty:
+        Display an alert modal (not a regular alert, confirm, or prompt!) to the user to input a movie title.
+        Exit and wait for the next action?
+
+    Otherwise, request movie info from OMDb using the movie title.
+
+        If the movie is found on OMDb:
+            Display movie info.
+            Save this move data to local storage for future reference.
+
+            Request soundtrack/playlist related to the movie from Spotify.
+
+            If a related playlist is found on Spotify:
+                Display the playlist info.
+                Save this playlist data to local storage for future reference.
+
+            Else:
+                Display a message that there's no related playlist found on Spotify.
+        Else:
+            Display a message that the movie was not found on OMDb.
+
+When 'search' button is clicked:
+    Call whenSearchButtonClicked()
+
+*/
